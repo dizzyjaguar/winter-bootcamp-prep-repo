8c43aa16cf87e99d702d752bfd326fd4b99de05d
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-08 20:49:22 -0800

    add script for nametag.html that will change the name with javascript

--- index-script.js
+++ index-script.js
@@ -1,1 +1,8 @@
-console.log('Hello this is my index page');
+const circle1 = document.getElementById('square-1');
+const circle2 = document.getElementById('square-2');
+
+circle1.textContent = 'This is Circle1';
+circle2.textContent = 'This is Circle2';
+
+const circleOneName = prompt('What should this cirlce be named?')
+
--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+
+// this is getting elements from my HTML code by name of the id and turning them into 
+// Javascript variables
+const nameBox = document.getElementById('name');
+
+const myInput = document.getElementById('get-name');
+
+const button = document.getElementById('name-button');
+
+// this is a function that is changing the textContent of the nameBox and
+// changing it to the value of the user input, 'myInput' which is the 
+// HTML elemement id 'get-name'
+
+const displayName = () => {
+    nameBox.textContent = myInput.value
+}
+
+// this makes the event of the function displayName to happen 
+// when we 'click' the button
+
+button.addEventListener('click', displayName)
