cbb88b451183264da77c406ecd4b8ffdf3a5dd63
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-09 19:04:01 -0800

    add conditionals for its past 5pm

--- script.js
+++ script.js
@@ -7,15 +7,30 @@ const myInput = document.getElementById('get-name');
 
 const button = document.getElementById('name-button');
 
+const greeting = document.getElementById('greeting');
+
+const nameIs = document.getElementById('name-is');
+
+const theTime = new Date().getHours();
+
 // this is a function that is changing the textContent of the nameBox and
 // changing it to the value of the user input, 'myInput' which is the 
 // HTML elemement id 'get-name'
 
 const displayName = () => {
-    nameBox.textContent = myInput.value
+    nameBox.textContentcol = myInput.value
 }
 
 // this makes the event of the function displayName to happen 
 // when we 'click' the button
 
+if (theTime > 17 ) {
+    greeting.style.color = "blue",
+    nameBox.style.visibility = "hidden";
+    nameIs.textContent = "its night time 'aka' past 5PM"
+    nameIs.style.color = "green"
+} 
+
+
 button.addEventListener('click', displayName)
+
